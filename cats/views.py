89c88e9bb7221d52d7711d7c92cabf8df14339e3
Андрей Полушин <pandser@yandex.ru>
2023-01-02 11:21:49 +0300

    пермишен, пагинатор, тротлиг, фильтры

--- cats/views.py
+++ cats/views.py
@@ -1,13 +1,34 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
+from rest_framework import filters
+from rest_framework.throttling import ScopedRateThrottle
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Achievement, Cat, User
-
+from .permissions import OwnerOrReadOnly, ReadOnly
+from .pagination import CatsPagination
 from .serializers import AchievementSerializer, CatSerializer, UserSerializer
+from .throttling import WorkingHoursRateThrottle
 
 
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
+    permission_classes = (OwnerOrReadOnly,)
+    throttle_classes = (ScopedRateThrottle, WorkingHoursRateThrottle)
+    throttle_scope = 'low_request'
+    pagination_class = CatsPagination
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter,
+                       filters.OrderingFilter)
+    filterset_fields = ('color', 'birth_year')
+    search_fields = ('name',)
+    ordering_fields = ('name', 'birth_year')
+    ordering = ('birth_year',)
+
+    def get_permissions(self):
+        if self.action == 'retrieve':
+            return (ReadOnly(),)
+        return super().get_permissions()
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user) 
@@ -16,6 +37,7 @@ class CatViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    pagination_class = LimitOffsetPagination
 
 
 class AchievementViewSet(viewsets.ModelViewSet):
